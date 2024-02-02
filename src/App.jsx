446581a593b2b07dbd7b488e1e65d359c28a1504
Youssef-faradi <faradiyoussef1@gmail.com>
2024-02-02 16:20:02 +0100

    update assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Home } from './pages/Home/home.jsx';
 import { Market } from './pages/Market/market.jsx';
 import { People } from './pages/People/people.jsx';
 import { Profile } from './pages/Profile/profile.jsx';
+import { Forme } from './pages/Forme/forme.jsx';
 import { Route, Routes } from 'react-router-dom';
 import { Error } from './pages/Errors/error.jsx';
 
@@ -15,6 +16,8 @@ function App() {
                 <Route path='/market' element={<Market/>} />
                 <Route path='/people' element={<People />} />
                 <Route path='/profile' element={<Profile />} />
+                <Route path='/form' element={<Forme />} />
+
 
 
             </Routes>
