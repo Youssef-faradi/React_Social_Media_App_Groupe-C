2e007373d592a19b1577544ee7afac7870c32ad9
Youssef-faradi <faradiyoussef1@gmail.com>
2024-02-05 20:27:31 +0100

    Home Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 /** @type {import(tailwindcss).Config} */
+const {nextui} = require("@nextui-org/react");
+
 module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {
-            "primary-color": "var(--primary-color)",
-            "secondary-color": "var(--secondary-color)"
+            "teal": "var(--teal)",
+            "red": "var(--red)",
+            "dark": "var(--dark)",
+            "light": "var(--light)",
+            "green": "var(--green)",
+            "lightGreen": "var(--lightGreen)"
+
         },
     },
-    plugins: [],
+    darkMode: "class",
+    plugins: [nextui()],
 }
